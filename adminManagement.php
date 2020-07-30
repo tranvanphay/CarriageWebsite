6b50c654e35666e97c3942aa5caaf2d1fc45167c
TranVanPhay <phay.tran99@gmail.com>
2020-07-30 17:16:25 +0700

    update

--- adminManagement.php
+++ adminManagement.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    echo "Welcome to the member's area, " . $_SESSION['username'] . "!";
+    echo "<script> window.location.assign('/adminPage.php'); </script>";
 } else {
     echo "<script> window.location.assign('/login.php'); </script>";
 }
